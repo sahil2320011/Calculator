759f72336457917209a104a82705d2b067f2ec3a
Sahil <sahilsharama232001@gmail.com>
2021-05-20 14:45:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenvalue='';
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttontext=e.target.innerText;
+        console.log('button text is',buttontext);
+        if(buttontext=='X'){
+            buttontext="*";
+            screenvalue+=buttontext;
+            screen.value=screenvalue;
+        }else if(buttontext=='C'){
+            screen.value="";
+            screenvalue="";
+        }else if(buttontext=='='){
+            screen.value=eval(screenvalue);
+            screenvalue=screen.value;
+        }else{
+            screenvalue+=buttontext;
+            screen.value=screenvalue;
+        }
+    })
+}
